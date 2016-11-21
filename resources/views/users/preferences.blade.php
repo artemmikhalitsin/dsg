f7aefd7ead1e5410a730f05f2e323bc73ad0329c
Kyerine <hyerine0@gmail.com>
2016-11-21 02:38:25 -0500

    more UI stuffs

--- resources/views/users/preferences.blade.php
+++ resources/views/users/preferences.blade.php
@@ -3,22 +3,24 @@
 @section('content')
 
 <div class="container">
-<div class="panel panel-primary">
-<div class="panel-heading">Preferences</div>
-<div class="panel-body">
-{!!  Form::open()   !!}
+	<div class="row">
+		<div class="col-md-10 col-md-offset-1">
+			<div class="panel panel-primary">
+				<div class="panel-heading">Preferences</div>
+					<div class="panel-body">
+						{!!  Form::open()   !!}
 
-  {!! Form::label('startTime', 'Start Time:') !!}<br>
-  {!! Form::time('startTime', 'NEVER') !!}
-<br><br>
-  {!! Form::label('endTime', 'End Time:') !!}<br>
-  {!! Form::time('startTime', 'NEVER') !!}
-<br><br>
-  {!! Form::label('courseLoad', 'Desired Course Load:') !!}<br>
-  {!! Form::select('size', array('1' => '1 course', '2' => '2 courses', '3' => '3 courses', '4' => '4 courses', '5' => '5 courses', '6' => '6 courses')); !!}
+						{!! Form::label('startTime', 'Start Time:') !!}<br>
+						{!! Form::time('startTime', 'NEVER') !!}<br><br>
+						{!! Form::label('endTime', 'End Time:') !!}<br>
+						{!! Form::time('startTime', 'NEVER') !!}<br><br>
+						{!! Form::label('courseLoad', 'Desired Course Load:') !!}<br>
+						{!! Form::select('size', array('1' => '1 course', '2' => '2 courses', '3' => '3 courses', '4' => '4 courses', '5' => '5 courses', '6' => '6 courses')); !!}
 
-{!! Form::close() !!}
-</div>
-</div>
+						{!! Form::close() !!}
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 @endsection
