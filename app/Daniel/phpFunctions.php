d5f0b9ce835775427c2998d177fedec22de29035
Wai-Lau <wailunlau@hotmail.com>
2016-11-21 16:59:04 -0500

    Started integration with Daniel's stuff

---
+++ app/Daniel/phpFunctions.php
@@ -0,0 +1,237 @@
+<?php
+
+include 'courses.php';
+include 'SequenceTree.php';
+/**
+*finds the last semester where there is room to plant a tree. Our semesters are the rows of $sequence
+*/
+function findLastEmptySemester($s) //initialized sequence as input
+{
+	$rows = (count($s)); //these are our total semeseters
+	$cols = (count($s[0])); //these are our slots per semester
+	$ret = $rows-1;
+	
+	for($a = $rows-1; $a >= 0; $a--)
+	{
+		for($b = (count($s[$a]))-1; $b >= 0; $b--)
+		{
+			//echo "a: $a\n";
+			//echo "b: $b\n";
+			if(($s[$a][$b]) == '')
+				return $ret;
+		}
+		$ret = $ret-1;
+	}
+	return 0; 
+}
+
+/**
+* Returns whether or not a course is in an array based on its id
+*/
+function inArrayCourse($c, $cl)
+{
+	for ($i=0; $i<count($cl); $i++)
+	{
+		if($c->getId() == $cl[$i]->id)
+			return true;
+	}
+	return false;
+}
+
+/**
+* Finds and replaces a course in an array
+*/
+function findAndReplace($c, $cl)
+{
+	for ($i=0; $i<count($cl); $i++)
+	{
+		if($c->id == $cl[$i]->id)
+			$cl[$i]=$c;
+	}
+	return $cl;
+}
+/**
+*assigns level to a single class and all of its children, and also generates subtrees. Meant to go in a for loop
+*/
+function assignLevel($c, $cl) //a course and the final list of courses to take
+{
+	echo "id: ".$c -> getId()." :: ";
+	$currentLevel = -1;
+	$prereqs = '';
+	$prereqs = $c -> getPrq(); //gets prerequisites
+	$c = (array)$c;
+	//$c['subtree']=array();
+	//$c['subtree'][0]= $c;
+	$c = (object)$c; //creates subtree
+	
+	if(empty($prereqs)) //if course is a leaf, it is treated as a leaf
+		$c -> level = 0;
+	else
+	{
+		foreach($prereqs as &$prq)  
+		{
+			echo $prq -> getId()." ";
+			if(!(inArrayCourse($prq, $cl))) 		//if its prerequisites are not meant to be taken (for example they were taken before)
+				;					//nothing happens. It will be treated as leaf because of the last line in the else
+			else if($prq ->level != -1) //if its prerequisite was already visited and got a height
+			{
+				$currentLevel=max($currentLevel, $prq->level); //level of parent must be higher
+				//$c['subtree']=array_merge($c['subtree'], $prq['subtree']); //its subtree is added to subtree
+			}
+			else //else the child are unvisited
+			{
+				$prq=assignLevel($prq, $cl); //get visited
+				$prereqs=findAndReplace($prq, $prereqs); //updates prerequisites
+				$currentLevel=max($currentLevel, $prq->level); //checks level
+				//$c['subtree']=array_merge($c['subtree'], $prq['subtree']); //subtrees added
+			}
+		}
+		$c -> level = ($currentLevel + 1); //level becomes +1 above highest child
+	}
+	$c -> prerequisites = $prereqs; //ipdates prerequisites
+	return $c;
+	
+}
+
+/*
+Ensures all corequisites must have the same height
+*/
+function assignCoLevel($c, $cl) //class and final list of courses
+{
+	echo "id: ".$c -> id." :: ";
+	$currentHeight = $c -> level;
+	$coreqs ='';
+	$coreqs = $c -> corequisites;
+	
+	if(empty($coreqs)) //if no coreqs nothing happens
+	{
+		;
+	}
+	else
+	{
+		$i = 0;
+		foreach($coreqs as &$crq) //finds tallest height
+		{
+			$currentHeight = max($currentHeight, $crq->level); 
+		}
+		foreach($coreqs as &$crq) //equalizes height of all corequisites
+		{
+			$crq ->level = ($currentHeight);
+			findAndReplace($crq, $coreqs);
+		}
+		$c -> corequisites = $coreqs;
+		$c -> level =($currentHeight);
+	}
+	
+	return $c;
+}
+
+
+function notContain($co, $seq)
+{
+	$rows=count($seq);
+	$courseId = $co -> id;
+	$objToTest;
+	
+	for ($a=0; $a<$rows; $a++){
+		//echo "\n a".$a." ";
+		for($b=0; $b<count($seq[$a]); $b++){
+			//echo "b".$b." ";
+			if(is_object($seq[$a][$b]))
+			{
+				if($courseId==($seq[$a][$b] -> id))
+					return false;
+			}
+		}
+	}
+	return true;
+}
+
+function sortByLevel($cl)
+{
+	for($i = 0; $i< count($cl); $i++)
+		$cl=sortaSorter($i, $cl);
+	
+	return $cl;
+}
+
+/**
+ * courses at index $i are placed in relative position to courses with indexes lower than $i based on their levels
+ */
+function sortaSorter($i, $cl) //position in array and array
+{
+	$placeholder ='';
+	if($i-1>=0)
+	{
+		if($cl[$i-1] -> level < $cl[$i] -> level) //higher levels go up.
+		{
+			$placeholder = $cl[$i-1];
+			$cl[$i-1] = $cl[$i];
+			$cl[$i] = $placeholder;
+			$cl = sortaSorter($i-1, $cl);
+		}
+	}
+	return $cl;
+	
+}
+function findNumberOfPlaces($seq, $sem)//sequence and semester
+{
+	//echo"\n offset? : ".$sem."\n";
+	$ret = 0;
+	for($b = 0; $b<count($seq[$sem]); $b++)
+	{
+		if(is_null($seq[$sem][$b]))
+			$ret += 1;
+	}
+	return $ret;
+}
+
+function plantTrees($start, $seq, $co) // semester, sequence, and current course
+{
+	$prereqs = $co -> prerequisites;
+	//$coreqs = $co -> corequisites;
+	$spotsOnLevel = findNumberOfPlaces($seq, $start);
+	$nextStart=$start-1; //semester for next iteration
+	if($start<0)
+	{
+		echo "error";
+		return null;
+	}
+	if(notContain($co, $seq)) //if the course is not already placed in sequence
+	{
+		//place course
+		if($spotsOnLevel>0) //if there is room in current semester
+		{
+			for($b = 0; $b<count($seq[$start]); $b++)  //find room and place course
+			{
+				if(is_null($seq[$start][$b]))
+				{
+					$seq[$start][$b]=$co;
+					$b=count($seq[$start]);
+				}
+			}
+		}
+		else if(findNumberOfPlaces($start+1, $start) >0) //if course has not been placed and there is room in a semester with its parent
+			$seq = plantTrees($start+1, $seq, $co);		
+		else //if there is only room bellow
+		{	
+			$seq=plantTrees($nextStart, $seq, $co);	
+			$nextStart=$nextStart-1; //decremented to ensure prereqs will then be properly placed
+		}
+			
+		//place prereqs
+		if(!empty($prereqs)) 
+		{	
+			foreach($prereqs as &$prq)
+			{
+				if($prq -> level >= $start) //if the level of the prq is higher the current semester, then it must be first attempted to be placed at the same level as its parent.
+					$seq=plantTrees($nextStart+1, $seq, $prq);
+				else
+					$seq=plantTrees($nextStart, $seq, $prq);
+			}	
+		}
+	}
+	return $seq;
+}	
+
+?>
--- app/SequenceTree.php
+++ app/SequenceTree.php
@@ -11,26 +11,11 @@ use DB;
 class SequenceTree extends Model
 {
   public static function getOutput(){
-    $userProgram = array('218','39');
+    $userProgram = array('174'); //,'218','39'
     $CourseList = [];
 
     foreach ($userProgram as $userCourse){
-
-    $courseInfo = DB::table('courses')
-      ->join('courseavailability','courses.course_id', '=','courseavailability.course_id')
-      ->join('prerequisites','prerequisites.course_id', '=','courses.course_id')
-      ->select('courses.course_code','courses.course_id','winter','fall','prerequisite')
-      ->where('courses.course_id','=',$userCourse)
-      ->get();
-
-    $CourseList[] = $course = new Course($userCourse);
-    $course->name = $courseInfo[0]->course_code;
-    $course->winter = $courseInfo[0]->winter;
-    $course->fall = $courseInfo[0]->fall;
-
-    //coursePrereq is actually not just a prereq, it's more info
-    foreach ($courseInfo as $coursePrereq)
-      $course->addPrerequisiteObject($coursePrereq->prerequisite, $CourseList);
+      $CourseList[] = $course = new Course ($userCourse, $CourseList);
     }
 
     return json_encode($CourseList);
@@ -43,14 +28,28 @@ class Course
   var $id;
   var $winter;
   var $fall;
-  var $prerequisites = array();
-  //var $corequisites = array();
-  //var $height = 0;
+  var $level = -1;
+  var $prerequisiteList = array();
 
-  function __construct($id) {
+  function __construct($id, &$CourseList) {
     $this->id = $id;
+    $courseInfo = DB::table('courses')
+      ->join('courseavailability','courses.course_id', '=','courseavailability.course_id')
+      ->join('prerequisites','prerequisites.course_id', '=','courses.course_id')
+      ->select('courses.course_code','courses.course_id','winter','fall','prerequisite')
+      ->where('courses.course_id','=',$id)
+      ->get();
+    $this->name = $courseInfo[0]->course_code;
+    $this->winter = $courseInfo[0]->winter;
+    $this->fall = $courseInfo[0]->fall;
+
+    //coursePrereq is actually not just a prereq, it's more info
+    foreach ($courseInfo as $coursePrereq){
+      $this->addPrerequisiteObject($coursePrereq->prerequisite, $CourseList);
+    }
   }
 
+  //adds a prerequisite object to the current course, checks if that object exists already
   function addPrerequisiteObject($prerequisiteId, &$CourseList){
 
     $courseInfo = DB::table('courses')
@@ -63,20 +62,81 @@ class Course
     $already = false;
     foreach ($CourseList as $course){
       if ($course->id == $prerequisiteId){
-        $this->prerequisites[] = $course;
+        $this->prerequisiteList[] = new Prerequisite($course);
         $already = true;
       }
     }
 
     if (!$already && !empty($courseInfo)){
-      $CourseList[] = $course = new Course ($prerequisiteId);
-      $course->name = $courseInfo[0]->course_code;
-      $course->winter = $courseInfo[0]->winter;
-      $course->fall = $courseInfo[0]->fall;
-      foreach ($courseInfo as $coursePrereq)
-        $course->addPrerequisiteObject($coursePrereq->prerequisite, $CourseList);
-      $this->prerequisites[] = $course;
+      $CourseList[] = $course = new Course ($prerequisiteId, $CourseList);
+      $this->prerequisiteList[] = new Prerequisite($course);
     }
+  }
+
+  function getPrq()
+  {
+    if (property_exists($this, 'prerequisiteList')) 
+      return $this->prerequisiteList;
+  }
+
+  function getId()
+  {
+    if (property_exists($this, 'id')) 
+      return $this->id;
+  }
+
+  function getLevel()
+  {
+    if (property_exists($this, 'level'))
+      return $this->level;
+  }
+
+  function setLevel($l)
+  {
+    if (property_exists($this, 'level'))
+      $this->level = $l;
+  }
+
+  /*
+  function __construct($id, $prq, $crq)
+  {
+    $this->id = $id;
+    $this->prerequisites = $prq;
+    $this->corequisites = $crq;
+    $this->level = -1;
+  }
+  
+  function addCrq($crq)
+  {
+    $this->corequisites[]=$crq;
+  }
+
+  function addPrq($prq)
+  {
+    $this->prerequisiteList[]->prerequisite=$prq;
+  }
+  
+  function setPrqs($prqs)
+  {
+    $this->prerequisiteList= $prqs;
+  }
+
+  function getCrq()
+  {
+    if (property_exists($this, 'corequisites')) 
+      return $this -> corequisites;
+    }
+  */
+}
+
+class Prerequisite
+{
+  var $isCorequisite = 0;
+  var $prerequisite;
+  var $orPrereq = Array();
+
+  function __construct($course){
+    $this->prerequisite = $course;
   }
 }
 
