d462a6bb387c87be38831d4f4f65fc8c8aae7535
AjmerSingh7537 <ajmer226@yahoo.ca>
2016-11-21 22:08:32 -0500

    displaying the completed courses using Datatable with a delete button inside the profile view

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.app')
 
+@section('extraCSS')
+    <!-- DataTables CSS -->
+    <link href="/css/dataTables/dataTables.bootstrap.css" rel="stylesheet">
+
+    <!-- DataTables Responsive CSS -->
+    <link href="/css/dataTables/dataTables.responsive.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="/css/dataTables/sb-admin-2.css" rel="stylesheet">
+@endsection
+
 @section('content')
 <div class="container">
     <div class="row">
@@ -18,15 +29,57 @@
                 </div>
 
             </div>
+         
             <div class="panel panel-primary">
-              <div class="panel-heading">Completed Courses</div>
-              <div class="panel-body">
-                    @foreach($completedCourses as $course)
-                        <button type="button" class="btn btn-success">{{ $course->course_code }}</button></br></br>
-                    @endforeach
-              </div>
-
+                <div class="panel-heading">
+                    List of Completed Courses
+                </div>
+                <!-- /.panel-heading -->
+                <div class="panel-body">
+                    <table width="100%" class="table table-striped table-bordered table-hover" id="dataTables-example">
+                        <thead>
+                            <tr>
+                                <th>Code</th>
+                                <th>Name</th>
+                                <th>Credits</th>
+                                <th>Option</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($completedCourses as $course)
+                            <tr class="odd gradeX">
+                                <td>{{ $course->course_code }}</td>
+                                <td>{{ $course->course_name }}</td>
+                                <td style="text-align: center;">{{ $course->credits }}</td>
+                                <td style="text-align: center;">
+                                    <a href="#" class="btn btn-danger btn-sm">
+                                    <span class="glyphicon glyphicon-remove-sign"></span> Delete</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    <!-- /.table-responsive -->
+                </div>
+                <!-- /.panel-body -->
+            </div>
         </div>
     </div>
 </div>
+
+@endsection
+
+@section('extraJS')
+    <!-- DataTables JavaScript -->
+    <script src="/js/dataTables/jquery.dataTables.min.js"></script>
+    <script src="/js/dataTables/dataTables.bootstrap.min.js"></script>
+    <script src="/js/dataTables/dataTables.responsive.js"></script>
+
+    <script>
+    $(document).ready(function() {
+        $('#dataTables-example').DataTable({
+            responsive: true
+        });
+    });
+    </script>
 @endsection
