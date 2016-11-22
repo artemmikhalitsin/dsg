f0d6111e2256e53527e8c79996cc82d5162b0c4e
Wai-Lau <wailunlau@hotmail.com>
2016-11-21 20:25:09 -0500

    orRequisites working

--- app/SequenceTree.php
+++ app/SequenceTree.php
@@ -12,7 +12,7 @@ class SequenceTree extends Model
 {
   //Everything starts here:
   public static function getOutput(){
-    $userProgram = array('174'); //,'218','39'
+    $userProgram = array('18'); //,'218','39','174'
     $CourseList = [];
 
     foreach ($userProgram as $userCourse){
@@ -26,11 +26,11 @@ class SequenceTree extends Model
 class Course
 {
   var $name;
+  var $prerequisiteList = array();
   var $id;
   var $winter;
   var $fall;
-  var $level = -1;
-  var $prerequisiteList = array();
+  var $level = -1;  
 
   function __construct($id, &$CourseList) {
     $this->id = $id;
@@ -63,24 +63,17 @@ class Course
   //adds a prerequisite object to the current course, checks if that object exists already
   function addPrerequisiteObject($prerequisiteCourseId, &$CourseList){
 
-    $courseInfo = DB::table('courses')
-    ->join('courseavailability','courses.course_id', '=','courseavailability.course_id')
-    ->join('prerequisites','prerequisites.course_id','=','courses.course_id')
-    ->select('courses.course_code','courses.course_id','winter','fall','prerequisite','prereq_id')
-    ->where('courses.course_id','=',$prerequisiteCourseId)
-    ->get();
-
     $already = false;
     foreach ($CourseList as $course){
       if ($course->id == $prerequisiteCourseId){
-        $this->prerequisiteList[] = new Prerequisite($course, $this->id);
+        $this->prerequisiteList[] = new Prerequisite($course, $this->id, $CourseList);
         $already = true;
       }
     }
 
-    if (!$already && !empty($courseInfo)){
-      $CourseList[] = $course = new Course ($prerequisiteCourseId, $CourseList);
-      $this->prerequisiteList[] = new Prerequisite($course, $this->id);
+    if (!$already){
+      $CourseList[] = $course = new Course ($prerequisiteCourseId, $CourseList, $CourseList);
+      $this->prerequisiteList[] = new Prerequisite($course, $this->id, $CourseList);
     }
   }
 
@@ -142,21 +135,57 @@ class Course
 
 class Prerequisite
 {
-  var $isCorequisite = 0;
-  var $prerequisite;
-  var $parent_id;
-  var $prereq_id;
-  var $orPrereq = Array();
+  //this is the list of requisites to complete this prerequisite
+  var $prerequisiteChoices = Array();
 
-  function __construct($prerequisite, $parent_id){
-    $this->prerequisite = $prerequisite;
+  //this is the id of the prerequisite
+  var $prereq_id;
+
+  var $isCorequisite = 0;
+
+  var $parent_id;
+
+  function __construct($prerequisite, $parent_id, &$CourseList){
+    $this->prerequisiteChoices[] = $prerequisite;
     $this->parent_id = $parent_id;
+    
     $prereqInfo = DB::table('prerequisites')
     ->select('prereq_id')
-    ->where('prerequisite','=', $prerequisite->id)
-    ->where('course_id','=', $parent_id)
+    ->where('prerequisite', '=', $prerequisite->id)
+    ->where('course_id', '=', $parent_id)
     ->get();
+
     $this->prereq_id = $prereqInfo[0]->prereq_id;
+
+    //this gets the other options
+    $this->getOrReq($CourseList);
+  }
+
+  function getOrReq(&$CourseList){
+    $orReqInfo = DB::table('courses')
+    ->join('prerequisites','prerequisites.course_id', '=','courses.course_id')
+    ->join('orprerequisites','prerequisites.prereq_id','=','orprerequisites.prereq_id')
+    ->select('orprerequisites.course_id')
+    ->where('orprerequisites.prereq_id','=',$this->prereq_id)
+    ->get();
+
+    //SELECT orprerequisites.course_id as orPrereq FROM courses JOIN prerequisites ON prerequisites.course_id = courses.course_id JOIN orprerequisites ON orprerequisites.prereq_id = prerequisites.prereq_id WHERE courses.course_id = 9
+
+    foreach ($orReqInfo as $orReq){
+      $already = false;
+      foreach ($CourseList as $course){
+        if ($course->id == $orReq->course_id){
+          $this->prerequisiteChoices[] = $course;
+          $already = true;
+        }
+      }
+      if (!$already){
+        $this->prerequisiteChoices[] = new Course ($orReq->course_id, $CourseList);
+      }
+    }
+
+
+
   }
 }
 
