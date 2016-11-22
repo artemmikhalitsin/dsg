721245bc04dd4df05269da0f710495f372a0c8cd
artemmikhalitsin@gmail.com <artemmikhalitsin@gmail.com>
2016-11-22 15:33:32 -0500

    Fixed schedule spacing issue

--- resources/views/courses/schedule.blade.php
+++ resources/views/courses/schedule.blade.php
@@ -9,145 +9,160 @@
 @endsection
 
 @section('content')
-<div id="schedule-reset">
-	<div id="schedule">
-		<div class="cd-schedule loading">
-			<div class="timeline">
-				<ul>
-					<li><span>09:00</span></li>
-					<li><span>09:30</span></li>
-					<li><span>10:00</span></li>
-					<li><span>10:30</span></li>
-					<li><span>11:00</span></li>
-					<li><span>11:30</span></li>
-					<li><span>12:00</span></li>
-					<li><span>12:30</span></li>
-					<li><span>13:00</span></li>
-					<li><span>13:30</span></li>
-					<li><span>14:00</span></li>
-					<li><span>14:30</span></li>
-					<li><span>15:00</span></li>
-					<li><span>15:30</span></li>
-					<li><span>16:00</span></li>
-					<li><span>16:30</span></li>
-					<li><span>17:00</span></li>
-					<li><span>17:30</span></li>
-					<li><span>18:00</span></li>
-					<li><span>18:30</span></li>
-					<li><span>19:00</span></li>
-					<li><span>19:30</span></li>
-					<li><span>20:00</span></li>
-					<li><span>20:30</span></li>
-					<li><span>21:00</span></li>
-					<li><span>21:30</span></li>
-					<li><span>22:00</span></li>
-					<li><span>22:30</span></li>
-				</ul>
-			</div> <!-- .timeline -->
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-primary">
+                <div class="panel-heading">Your Schedule</div>
 
-			<div class="events">
-				<ul>
-					<li class="events-group">
-						<div class="top-info"><span>Monday</span></div>
-						@foreach($schedule[0] as $timeslot)
-						<ul>
-							<li class="single-event" data-start="{{$timeslot["start_time"]}}" data-end="{{$timeslot["end_time"]}}" data-content="{{$timeslot["type"]}}" data-event="event-1">
-								<a href="#0">
-									<em class="event-name">{{$timeslot["course_code"]}}<br>{{$timeslot["course_name"]}}</em>
-								</a>
-							</li>
-						</ul>
-						@endforeach
-					</li>
+                <div class="panel-body" style="padding-bottom:65%;">
+                    <div id="schedule-reset">
+                    	<div id="schedule">
+                    		<div class="cd-schedule loading">
+                    			<div class="timeline">
+                    				<ul>
+                                             <li><span>08:00</span></li>
+                    					<li><span>08:30</span></li>
+                    					<li><span>09:00</span></li>
+                    					<li><span>09:30</span></li>
+                    					<li><span>10:00</span></li>
+                    					<li><span>10:30</span></li>
+                    					<li><span>11:00</span></li>
+                    					<li><span>11:30</span></li>
+                    					<li><span>12:00</span></li>
+                    					<li><span>12:30</span></li>
+                    					<li><span>13:00</span></li>
+                    					<li><span>13:30</span></li>
+                    					<li><span>14:00</span></li>
+                    					<li><span>14:30</span></li>
+                    					<li><span>15:00</span></li>
+                    					<li><span>15:30</span></li>
+                    					<li><span>16:00</span></li>
+                    					<li><span>16:30</span></li>
+                    					<li><span>17:00</span></li>
+                    					<li><span>17:30</span></li>
+                    					<li><span>18:00</span></li>
+                    					<li><span>18:30</span></li>
+                    					<li><span>19:00</span></li>
+                    					<li><span>19:30</span></li>
+                    					<li><span>20:00</span></li>
+                    					<li><span>20:30</span></li>
+                    					<li><span>21:00</span></li>
+                    					<li><span>21:30</span></li>
+                    					<li><span>22:00</span></li>
+                    					<li><span>22:30</span></li>
 
-					<li class="events-group">
-						<div class="top-info"><span>Tuesday</span></div>
-						@foreach($schedule[1] as $timeslot)
-						<ul>
-							<li class="single-event" data-start="{{$timeslot["start_time"]}}" data-end="{{$timeslot["end_time"]}}" data-content="{{$timeslot["type"]}}" data-event="event-1">
-								<a href="#0">
-									<em class="event-name">{{$timeslot["course_code"]}}<br>{{$timeslot["course_name"]}}</em>
-								</a>
-							</li>
-						</ul>
-						@endforeach
-					</li>
+                    				</ul>
+                    			</div> <!-- .timeline -->
 
-					<li class="events-group">
-						<div class="top-info"><span>Wednesday</span></div>
-						@foreach($schedule[2] as $timeslot)
-						<ul>
-							<li class="single-event" data-start="{{$timeslot["start_time"]}}" data-end="{{$timeslot["end_time"]}}" data-content="{{$timeslot["type"]}}" data-event="event-1">
-								<a href="#0">
-									<em class="event-name">{{$timeslot["course_code"]}}<br>{{$timeslot["course_name"]}}</em>
-								</a>
-							</li>
-						</ul>
-						@endforeach
-					</li>
+                    			<div class="events">
+                    				<ul>
+                    					<li class="events-group">
+                    						<div class="top-info"><span>Monday</span></div>
+                    						@foreach($schedule[0] as $timeslot)
+                    						<ul>
+                    							<li class="single-event" data-start="{{$timeslot["start_time"]}}" data-end="{{$timeslot["end_time"]}}" data-content="{{$timeslot["type"]}}" data-event="event-1">
+                    								<a href="#0">
+                    									<em class="event-name">{{$timeslot["course_code"]}}<br>{{$timeslot["course_name"]}}</em>
+                    								</a>
+                    							</li>
+                    						</ul>
+                    						@endforeach
+                    					</li>
 
-					<li class="events-group">
-						<div class="top-info"><span>Thursday</span></div>
-						@foreach($schedule[3] as $timeslot)
-						<ul>
-							<li class="single-event" data-start="{{$timeslot["start_time"]}}" data-end="{{$timeslot["end_time"]}}" data-content="{{$timeslot["type"]}}" data-event="event-1">
-								<a href="#0">
-									<em class="event-name">{{$timeslot["course_code"]}}<br>{{$timeslot["course_name"]}}</em>
-								</a>
-							</li>
-						</ul>
-						@endforeach
-					</li>
+                    					<li class="events-group">
+                    						<div class="top-info"><span>Tuesday</span></div>
+                    						@foreach($schedule[1] as $timeslot)
+                    						<ul>
+                    							<li class="single-event" data-start="{{$timeslot["start_time"]}}" data-end="{{$timeslot["end_time"]}}" data-content="{{$timeslot["type"]}}" data-event="event-1">
+                    								<a href="#0">
+                    									<em class="event-name">{{$timeslot["course_code"]}}<br>{{$timeslot["course_name"]}}</em>
+                    								</a>
+                    							</li>
+                    						</ul>
+                    						@endforeach
+                    					</li>
 
-					<li class="events-group">
-						<div class="top-info"><span>Friday</span></div>
-						@foreach($schedule[4] as $timeslot)
-						<ul>
-							<li class="single-event" data-start="{{$timeslot["start_time"]}}" data-end="{{$timeslot["end_time"]}}" data-content="{{$timeslot["type"]}}" data-event="event-1">
-								<a href="#0">
-									<em class="event-name">{{$timeslot["course_code"]}}<br>{{$timeslot["course_name"]}}</em>
-								</a>
-							</li>
-						</ul>
-						@endforeach
-					</li>
+                    					<li class="events-group">
+                    						<div class="top-info"><span>Wednesday</span></div>
+                    						@foreach($schedule[2] as $timeslot)
+                    						<ul>
+                    							<li class="single-event" data-start="{{$timeslot["start_time"]}}" data-end="{{$timeslot["end_time"]}}" data-content="{{$timeslot["type"]}}" data-event="event-1">
+                    								<a href="#0">
+                    									<em class="event-name">{{$timeslot["course_code"]}}<br>{{$timeslot["course_name"]}}</em>
+                    								</a>
+                    							</li>
+                    						</ul>
+                    						@endforeach
+                    					</li>
 
-					<li class="events-group">
-						<div class="top-info"><span>Saturday</span></div>
-						@foreach($schedule[5] as $timeslot)
-						<ul>
-							<li class="single-event" data-start="{{$timeslot["start_time"]}}" data-end="{{$timeslot["end_time"]}}" data-content="{{$timeslot["type"]}}" data-event="event-1">
-								<a href="#0">
-									<em class="event-name">{{$timeslot["course_code"]}}<br>{{$timeslot["course_name"]}}</em>
-								</a>
-							</li>
-						</ul>
-						@endforeach
-					</li>
-				</ul>
-			</div>
+                    					<li class="events-group">
+                    						<div class="top-info"><span>Thursday</span></div>
+                    						@foreach($schedule[3] as $timeslot)
+                    						<ul>
+                    							<li class="single-event" data-start="{{$timeslot["start_time"]}}" data-end="{{$timeslot["end_time"]}}" data-content="{{$timeslot["type"]}}" data-event="event-1">
+                    								<a href="#0">
+                    									<em class="event-name">{{$timeslot["course_code"]}}<br>{{$timeslot["course_name"]}}</em>
+                    								</a>
+                    							</li>
+                    						</ul>
+                    						@endforeach
+                    					</li>
 
-			<div class="event-modal">
-				<header class="header">
-					<div class="content">
-						<span class="event-date"></span>
-						<h3 class="event-name"></h3>
-					</div>
+                    					<li class="events-group">
+                    						<div class="top-info"><span>Friday</span></div>
+                    						@foreach($schedule[4] as $timeslot)
+                    						<ul>
+                    							<li class="single-event" data-start="{{$timeslot["start_time"]}}" data-end="{{$timeslot["end_time"]}}" data-content="{{$timeslot["type"]}}" data-event="event-1">
+                    								<a href="#0">
+                    									<em class="event-name">{{$timeslot["course_code"]}}<br>{{$timeslot["course_name"]}}</em>
+                    								</a>
+                    							</li>
+                    						</ul>
+                    						@endforeach
+                    					</li>
 
-					<div class="header-bg"></div>
-				</header>
+                    					<li class="events-group">
+                    						<div class="top-info"><span>Saturday</span></div>
+                    						@foreach($schedule[5] as $timeslot)
+                    						<ul>
+                    							<li class="single-event" data-start="{{$timeslot["start_time"]}}" data-end="{{$timeslot["end_time"]}}" data-content="{{$timeslot["type"]}}" data-event="event-1">
+                    								<a href="#0">
+                    									<em class="event-name">{{$timeslot["course_code"]}}<br>{{$timeslot["course_name"]}}</em>
+                    								</a>
+                    							</li>
+                    						</ul>
+                    						@endforeach
+                    					</li>
+                    				</ul>
+                    			</div>
 
-				<div class="body">
-					<div class="event-info"></div>
-					<div class="body-bg"></div>
-				</div>
+                    			<div class="event-modal">
+                    				<header class="header">
+                    					<div class="content">
+                    						<span class="event-date"></span>
+                    						<h3 class="event-name"></h3>
+                    					</div>
 
-				<a href="#0" class="close">Close</a>
-			</div>
+                    					<div class="header-bg"></div>
+                    				</header>
 
-			<div class="cover-layer"></div>
-		</div> <!-- .cd-schedule -->
-	</div>
+                    				<div class="body">
+                    					<div class="event-info"></div>
+                    					<div class="body-bg"></div>
+                    				</div>
+
+                    				<a href="#0" class="close">Close</a>
+                    			</div>
+
+                    			<div class="cover-layer"></div>
+                    		</div> <!-- .cd-schedule -->
+                    	</div>
+                    </div>
+               </div>
+          </div>
+     </div>
+     </div>
 </div>
 
 @endsection
