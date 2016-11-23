5aab311ac2d1bdc02d57ecc9d18e2e5da9015857
Ricardo Liganor <Ricardo Liganor>
2016-11-22 22:47:22 -0500

    matched the preferences in profile view to the preferences page

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -35,16 +35,9 @@
               <div class="panel panel-primary" style="padding-bottom:4%">
                    <div class="panel-heading">Preferences</div>
                         <div class="panel-body">
-                             {!!  Form::open()   !!}
-
-                             {!! Form::label('startTime', 'Start Time:') !!}<br>
-                             {!! Form::time('startTime', 'NEVER') !!}<br><br>
-                             {!! Form::label('endTime', 'End Time:') !!}<br>
-                             {!! Form::time('startTime', 'NEVER') !!}<br><br>
-                             {!! Form::label('courseLoad', 'Desired Course Load:') !!}<br>
-                             {!! Form::select('size', array('1' => '1 course', '2' => '2 courses', '3' => '3 courses', '4' => '4 courses', '5' => '5 courses', '6' => '6 courses')); !!}
-
-                             {!! Form::close() !!}
+                          {!!  Form::open(['url' => '', 'class' => 'form-horizontal'])   !!}
+                            @include('users.preferences_form')
+                          {!! Form::close() !!}
                    </div>
               </div>
          </div>
